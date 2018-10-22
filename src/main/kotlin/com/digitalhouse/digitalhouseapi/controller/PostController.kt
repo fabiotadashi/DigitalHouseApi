32c2c9c7f32f51636c3d88ff4652a01ca37b870d
Ernanni <ernannicf@gmail.com>
2018-10-22 17:12:05 -0300

    Mudança do banco de imagens

--- src/main/kotlin/com/digitalhouse/digitalhouseapi/controller/PostController.kt
+++ src/main/kotlin/com/digitalhouse/digitalhouseapi/controller/PostController.kt
@@ -13,10 +13,10 @@ class PostController {
     @GetMapping
     fun get(): List<PostDTO> {
         return arrayListOf(
-                PostDTO("Nova turma iniciada","Em setembro começou a nova turma de Marketing Digital", LocalDateTime.now(),"Jose da Silva","http://www.meioemensagem.com.br/wp-content/uploads/2018/05/SalaAula_575.png"),
-                PostDTO("Cardápio do Gran Coffee Atualizado","No espaço de co-working existe um café com diversas opções.", LocalDateTime.now(),"João Ribeiro","https://www.digitalhouse.com/wp-content/uploads/2016/11/campus_img7.jpg"),
-                PostDTO("O poder da nova educação","Tudo está acelerado. As mudanças vêm em ondas, os conceitos sobrepõem-se e é difícil definir o que é temporário, modismo, do que é efetivamente novo e duradouro.", LocalDateTime.now(),"Maria Souza","http://vmv.group/wp-content/uploads/2018/07/IMG_8619-2.jpg"),
-                PostDTO("Digital Summit em breve","O digital summit reunirá diversos palestrantes na Digital House", LocalDateTime.now(),"Jose da Silva","https://www.digitalhouse.com/wp-content/uploads/2017/11/digital-su-643697.png")
+                PostDTO("Nova turma iniciada","Em setembro começou a nova turma de Marketing Digital", LocalDateTime.now(),"Jose da Silva","https://imgur.com/Ci9D35a"),
+                PostDTO("Cardápio do Gran Coffee Atualizado","No espaço de co-working existe um café com diversas opções.", LocalDateTime.now(),"João Ribeiro","https://imgur.com/Yi8C91Z"),
+                PostDTO("O poder da nova educação","Tudo está acelerado. As mudanças vêm em ondas, os conceitos sobrepõem-se e é difícil definir o que é temporário, modismo, do que é efetivamente novo e duradouro.", LocalDateTime.now(),"Maria Souza","https://imgur.com/i4Tmmuh"),
+                PostDTO("Digital Summit em breve","O digital summit reunirá diversos palestrantes na Digital House", LocalDateTime.now(),"Jose da Silva","https://imgur.com/xxIXUDu")
         )
     }
 
